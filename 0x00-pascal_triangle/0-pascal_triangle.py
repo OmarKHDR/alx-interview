dd4816f2b7ef181ee40a7308d9af09d1da425d16
OmarKHDR <omarsliman37@gmail.com>
2024-10-03 16:24:04 +0300

    pascal triangle is easy

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,32 +2,34 @@
 """
 pascal triangle
 """
-triangle = [[1],]
-
 
 def create_pascal(arr, begin, end):
-	"""
-	create each level by recurssion
-	"""
-    if end == 0:
-        return
+    """
+    create each level by recurssion
+    """
     if begin == end:
-        triangle[end].append(1)
+        arr[end].append(1)
         return
     elif begin == 0:
-        triangle.append([])
-        triangle[end].append(1)
+        arr.append([])
+        arr[end].append(1)
     elif end > 1:
-        triangle[end].append(arr[end-1][begin-1] + arr[end-1][begin])
+        arr[end].append(arr[end-1][begin-1] + arr[end-1][begin])
     create_pascal(arr, begin + 1, end)
 
 
 def pascal_triangle(n):
-	""" a function looping through each level of pascal triangle
-		return: the final triangle
-	"""
+    """ a function looping through each level of pascal triangle
+        return: the final triangle
+    """
+    triangle = [[],]
     if len(triangle) >= n:
         return triangle[:n]
     for i in range(n):
         create_pascal(triangle, 0, i)
     return triangle
+
+if __name__ == '__main__':
+    print(pascal_triangle(5))
+    print(pascal_triangle(3))
+    print(pascal_triangle(10))
