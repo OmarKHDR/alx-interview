504fe47d57bd9b32e9bd7ec523338e736417eb03
OmarKHDR <omarsliman37@gmail.com>
2024-10-10 23:40:53 +0300

    pls

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,6 +3,18 @@
 '''
 
 
+def recurse(queue, boxes, curkeys):
+    '''a function to recursively try to add
+    the boxes in the open order in queue
+    '''
+    for i in curkeys:
+        if i >= len(boxes):
+            continue
+        if boxes[i] not in queue:
+            queue.append(boxes[i])
+            recurse(queue, boxes, boxes[i])
+
+
 def canUnlockAll(boxes):
     '''Checks if all the boxes in a list of boxes containing the keys
     (indices) to other boxes can be unlocked given that the first
@@ -10,15 +22,12 @@ def canUnlockAll(boxes):
     '''
     length = len(boxes)
     unlocked = {0}
-    queue = []
-    for i in boxes:
-        if i not in queue:
-            queue.append(i)
-            for j in i:
-                if boxes[j] not in queue:
-                    queue.append(boxes[j])
+    queue = [boxes[0]]
+    recurse(queue, boxes, boxes[0])
     for i in queue:
         if boxes.index(i) in unlocked:
             for j in i:
+                if j >= len(boxes):
+                    continue
                 unlocked.add(j)
     return len(unlocked) == length
---
+++ 0x01-lockboxes/try.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''A module for working with lockboxes.
+'''
+
+
+def canUnlockAll(boxes):
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
+
+print(canUnlockAll([ [7, 5], [1, 10, 7], [9, 6, 10], [7, 9], [2], [7, 3], [7, 9, 10, 10, 8, 9, 2, 5], [7, 2, 2, 4, 4, 2, 4, 8, 7], [4, 2, 9, 6, 6, 5, 5], ]
+))
