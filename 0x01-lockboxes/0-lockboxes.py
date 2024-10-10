136de8fb63dd019c8d46c80bf12bb269b5e2506d
OmarKHDR <omarsliman37@gmail.com>
2024-10-10 22:56:46 +0300

    pls

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+
+def canUnlockAll(boxes):
+    length = len(boxes)
+    unlocked = {0}
+    queue = []
+    for i in boxes:
+        if i not in queue:
+            queue.append(i)
+            for j in i:
+                if boxes[j] not in queue:
+                    queue.append(boxes[j])
+    for i in queue:
+        if boxes.index(i) in unlocked:
+            for j in i:
+                unlocked.add(j)
+    return len(unlocked) == length
