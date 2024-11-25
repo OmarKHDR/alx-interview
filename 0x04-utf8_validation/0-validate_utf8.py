fee8123fbc0a931ec5101a6ea72e38ce7fbf2e9f
OmarKHDR <omarsliman37@gmail.com>
2024-11-25 12:40:10 +0200

    hahsasfad

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,7 +12,6 @@ def validUTF8(data):
     check = 0b10000000
     bit_limit = control_byte & check
     byte_count = 1
-    index = 0
     while (bit_limit != 0) and byte_count <= 6:
         check = check >> 1
         bit_limit = control_byte & check
@@ -22,6 +21,6 @@ def validUTF8(data):
     if byte_count == 1:
         return validUTF8(data[1:])
     for i in range(1, byte_count-1):
-        if data[index + i] & 0b11000000 != 0b10000000:
+        if data[i] & 0b11000000 != 0b10000000:
             return False
     return validUTF8(data[byte_count - 1:])
