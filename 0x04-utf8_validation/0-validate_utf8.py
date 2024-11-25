77612e8b5b2774ea9150972bcf650ab6a1e0780f
OmarKHDR <omarsliman37@gmail.com>
2024-11-25 11:38:34 +0200

    solved i guess

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''Module should be documented
+'''
+
+
+def validUTF8(data):
+    '''Func is Documented
+    '''
+    if data == []:
+        return True
+    control_byte = data[0]
+    check = 0b10000000
+    bit_limit = control_byte & check
+    byte_count = 1
+    index = 0
+    while (bit_limit != 0) and byte_count <= 6:
+        check = check >> 1
+        bit_limit = control_byte & check
+        byte_count += 1
+    if byte_count == 6 or byte_count == 2:
+        return False
+    if byte_count == 1:
+        return validUTF8(data[1:])
+    for i in range(byte_count):
+        if data[index + i] & 0b11000000 != 0b10000000:
+            return False
+    return validUTF8(data[byte_count - 1:])
